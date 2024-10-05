b0092d700ad7d9f221fb9bb5033bd76665801e24
Matteo <matteo.rossi1902@gmail.com>
2024-10-05 11:42:49 +0200

    Refactor ServiceTypeDAO to remove unused method findServiceTypesWithServicesByRestaurant

--- greedys_api/src/main/java/com/application/persistence/dao/Restaurant/ServiceTypeDAO.java
+++ greedys_api/src/main/java/com/application/persistence/dao/Restaurant/ServiceTypeDAO.java
@@ -12,8 +12,6 @@ import com.application.persistence.model.reservation.ServiceType;
 @Transactional(readOnly = true)
 @Repository
 public interface ServiceTypeDAO extends JpaRepository<ServiceType, Long> {
-	@Query(value = "SELECT st FROM ServiceType st WHERE EXISTS (SELECT s FROM Service s WHERE s.serviceType.id = st.id AND s.restaurant.id = :id)")
-	List<ServiceType> findServiceTypesWithServicesByRestaurant(Long id);
 
     ServiceType findByName(String name);
 }
