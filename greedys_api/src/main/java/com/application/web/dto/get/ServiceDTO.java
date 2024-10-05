dcbfc6797e92e90d5d70f0c6d9d421ca560f4005
Matteo <matteo.rossi1902@gmail.com>
2024-10-05 11:29:21 +0200

    Refactor ServiceDTO to handle multiple service types when creating a new service

--- greedys_api/src/main/java/com/application/web/dto/get/ServiceDTO.java
+++ greedys_api/src/main/java/com/application/web/dto/get/ServiceDTO.java
@@ -1,19 +1,21 @@
 package com.application.web.dto.get;
 
 import com.application.persistence.model.reservation.Service;
+import com.application.web.dto.ServiceTypeDto;
+import java.util.Collection;
 
 public class ServiceDTO {
     long id;
     String name;
     long restaurantId;
-    long serviceType;
+    Collection<ServiceTypeDto> serviceType;
     String info;
 
     public ServiceDTO(Service service){
         this.id = service.getId();
         this.name = service.getName();
         this.restaurantId = service.getRestaurant().getId();
-        this.serviceType = service.getServiceType().getId();
+        this.serviceType = service.getServiceType().stream().map(ServiceTypeDto::new).toList();
         this.info = service.getInfo();
     }
 
@@ -29,7 +31,7 @@ public class ServiceDTO {
         return restaurantId;
     }
 
-    public long getServiceType() {
+    public Collection<ServiceTypeDto> getServiceType() {
         return serviceType;
     }
 
