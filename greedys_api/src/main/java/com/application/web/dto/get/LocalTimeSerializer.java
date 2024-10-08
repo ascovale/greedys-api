5c22bf4d2a30d89af581470dc8618e57adf26af5
Matteo <matteo.rossi1902@gmail.com>
2024-10-08 18:33:30 +0200

    Refactor LocalTimeSerializer and LocalTimeDeserializer to use DateTimeFormatter for serialization and deserialization

--- greedys_api/src/main/java/com/application/web/dto/get/LocalTimeSerializer.java
+++ greedys_api/src/main/java/com/application/web/dto/get/LocalTimeSerializer.java
@@ -1,21 +1,19 @@
 package com.application.web.dto.get;
 
-import java.io.IOException;
-import java.time.LocalTime;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class LocalTimeSerializer extends JsonSerializer<LocalTime> {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
     @Override
     public void serialize(LocalTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeStartObject();
-        gen.writeNumberField("hour", value.getHour());
-        gen.writeNumberField("minute", value.getMinute());
-        gen.writeNumberField("second", value.getSecond());
-        gen.writeNumberField("nano", value.getNano());
-        gen.writeEndObject();
+        gen.writeString(value.format(formatter));
     }
 }
--- greedys_api/src/main/java/com/application/web/dto/post/LocalTimeDeserializer.java
+++ greedys_api/src/main/java/com/application/web/dto/post/LocalTimeDeserializer.java
@@ -2,24 +2,21 @@ package com.application.web.dto.post;
 
 import java.io.IOException;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonNode;
 
 public class LocalTimeDeserializer extends JsonDeserializer<LocalTime> {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
     @Override
     public LocalTime deserialize(JsonParser p, DeserializationContext ctxt)
             throws IOException, JsonProcessingException {
-        JsonNode node = p.getCodec().readTree(p);
-        int hour = node.get("hour").asInt();
-        int minute = node.get("minute").asInt();
-        int second = node.get("second").asInt();
-        int nano = node.get("nano").asInt();
-        return LocalTime.of(hour, minute, second, nano);
+        String time = p.getText();
+        return LocalTime.parse(time, formatter);
     }
 }
-
