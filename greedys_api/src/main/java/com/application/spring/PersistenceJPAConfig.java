54118469bda49666d700c63f818c4d912b30bc70
Matteo <matteo.rossi1902@gmail.com>
2024-12-02 12:12:53 +0100

    Rename mail_password to email_password in docker-compose.yml and set entityManagerFactory as primary in PersistenceJPAConfig

--- greedys_api/src/main/java/com/application/spring/PersistenceJPAConfig.java
+++ greedys_api/src/main/java/com/application/spring/PersistenceJPAConfig.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 import jakarta.persistence.EntityManagerFactory;
@@ -45,6 +45,7 @@ public class PersistenceJPAConfig {
     }
 
     @Bean
+    @Primary
     LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource);
