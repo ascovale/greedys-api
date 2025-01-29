a91885d40df7eca95bb8b57c6fa3a1f6ec7d9f46
Valentino <ascolesevalentino@gmail.com>
2025-01-29 21:59:23 +0100

    Refactor ReservationRestaurantController to improve method clarity and add new endpoints for accepting, rejecting, marking no-show, and marking seated reservations; update constructor for dependency injection.

--- greedys_api/src/main/java/com/application/controller/restaurant/ReservationRestaurantController.java
+++ greedys_api/src/main/java/com/application/controller/restaurant/ReservationRestaurantController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.application.persistence.model.reservation.Reservation;
 import com.application.persistence.model.restaurant.Restaurant;
 import com.application.persistence.model.user.User;
 import com.application.service.ReservationService;
@@ -28,10 +27,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * The ReservationRestaurantController class is responsible for handling HTTP
- * requests
- * related to reservations for the restaurant.
- * It contains methods for booking, creating reservations, retrieving
- * reservation lists, and managing the calendar.
+ * requests related to reservations for the restaurant.
+ * It contains methods for creating, accepting, rejecting, marking as no-show, and marking as seated reservations.
  */
 @Controller
 @RequestMapping({"/restaurant","/admin/restaurant"})
@@ -39,27 +36,43 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Reservation Restaurant", description = "APIs for managing reservations from the restaurant")
 public class ReservationRestaurantController {
 
-	@Autowired
 	private ReservationService reservationService;
 
 	@Autowired
-	private ResourceLoader resourceLoader;
+	public ReservationRestaurantController(ReservationService reservationService) {
+		this.reservationService = reservationService;
+	}
 
+	/**
+	 * Creates a new reservation.
+	 *
+	 * @param idRestaurant the ID of the restaurant
+	 * @param DTO the data transfer object containing reservation details
+	 * @return ResponseEntity indicating the result of the operation
+	 */
 	@Operation(summary = "Create a new reservation", description = "Endpoint to create a new reservation")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Reservation created successfully"),
 			@ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
 			@ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
 	})
-	@PostMapping("/{idRestaurant}/new_reservation}")
+	@PostMapping("/{idRestaurant}/new_reservation")
 	@PreAuthorize("@securityService.hasRestaurantUserPermissionOnRestaurantWithId(#idRestaurant) or hasRole('ADMIN')")
 	public ResponseEntity<?> createReservation(@PathVariable Long idRestaurant,
-			@RequestBody NewReservationDTO DTO) {
-				//TODO verificare NEWRESERVATIONDTO
+											   @RequestBody NewReservationDTO DTO) {
+		//TODO verificare NEWRESERVATIONDTO
 		reservationService.createReservation(DTO, getCurrentRestaurant());
 		return ResponseEntity.ok().build();
 	}
 
+	/**
+	 * Accepts a reservation by its ID.
+	 *
+	 * @param idRestaurant the ID of the restaurant
+	 * @param reservationId the ID of the reservation
+	 * @param accepted the acceptance status
+	 * @return ResponseEntity indicating the result of the operation
+	 */
 	@Operation(summary = "Accept a reservation", description = "Endpoint to accept a reservation by its ID")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Reservation accepted successfully"),
@@ -69,19 +82,34 @@ public class ReservationRestaurantController {
 	})
 	@PutMapping("/{idRestaurant}/reservation/{reservationId}/accept")
 	@PreAuthorize("@securityService.hasRestaurantUserPermissionOnRestaurantWithId(#idRestaurant) or hasRole('ADMIN')")
-	public ResponseEntity<?> acceptReservation(@PathVariable Long idRestaurant,@PathVariable Long reservationId, @RequestParam Boolean accepted) {
+	public ResponseEntity<?> acceptReservation(@PathVariable Long idRestaurant, @PathVariable Long reservationId, @RequestParam Boolean accepted) {
 		reservationService.markReservationAccepted(idRestaurant, reservationId, accepted);
-
 		return ResponseEntity.ok().build();
 	}
 
+	/**
+	 * Rejects a reservation by its ID.
+	 *
+	 * @param idRestaurant the ID of the restaurant
+	 * @param reservationId the ID of the reservation
+	 * @param rejected the rejection status
+	 * @return ResponseEntity indicating the result of the operation
+	 */
 	@Operation(summary = "Reject a reservation", description = "Endpoint to reject a reservation by its ID")
 	@PutMapping("/{idRestaurant}/reservation/{reservationId}/reject")
-	public ResponseEntity<?> rejectReservation(@PathVariable Long idRestaurant,@PathVariable Long reservationId, @RequestParam Boolean rejected) {
+	public ResponseEntity<?> rejectReservation(@PathVariable Long idRestaurant, @PathVariable Long reservationId, @RequestParam Boolean rejected) {
 		reservationService.markReservationRejected(idRestaurant, reservationId, rejected);
 		return ResponseEntity.ok().build();
 	}
 
+	/**
+	 * Marks a reservation as no show by its ID.
+	 *
+	 * @param idRestaurant the ID of the restaurant
+	 * @param reservationId the ID of the reservation
+	 * @param noShow the no-show status
+	 * @return ResponseEntity indicating the result of the operation
+	 */
 	@Operation(summary = "Mark a reservation as no show", description = "Endpoint to mark a reservation as no show by its ID")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Reservation marked as no show successfully"),
@@ -91,11 +119,19 @@ public class ReservationRestaurantController {
 	})
 	@PutMapping("/{idRestaurant}/reservation/{reservationId}/no-show")
 	@PreAuthorize("@securityService.hasRestaurantUserPermissionOnRestaurantWithId(#idRestaurant) or hasRole('ADMIN')")
-	public ResponseEntity<?> markReservationNoShow(@PathVariable Long idRestaurant,@PathVariable Long reservationId, @RequestParam Boolean noShow) {
-		reservationService.markReservationNoShow(idRestaurant,reservationId, noShow);
+	public ResponseEntity<?> markReservationNoShow(@PathVariable Long idRestaurant, @PathVariable Long reservationId, @RequestParam Boolean noShow) {
+		reservationService.markReservationNoShow(idRestaurant, reservationId, noShow);
 		return ResponseEntity.ok().build();
 	}
 
+	/**
+	 * Marks a reservation as seated by its ID.
+	 *
+	 * @param idRestaurant the ID of the restaurant
+	 * @param reservationId the ID of the reservation
+	 * @param seated the seated status
+	 * @return ResponseEntity indicating the result of the operation
+	 */
 	@Operation(summary = "Mark a reservation as seated", description = "Endpoint to mark a reservation as seated by its ID")
 	@ApiResponses(value = {
 			@ApiResponse(responseCode = "200", description = "Reservation marked as seated successfully"),
@@ -105,11 +141,16 @@ public class ReservationRestaurantController {
 	})
 	@PutMapping("/{idRestaurant}/reservation/{reservationId}/seated")
 	@PreAuthorize("@securityService.hasRestaurantUserPermissionOnRestaurantWithId(#idRestaurant) or hasRole('ADMIN')")
-	public ResponseEntity<?> markReservationSeated(@PathVariable Long idRestaurant,@PathVariable Long reservationId, @RequestParam Boolean seated) {
-		reservationService.markReservationSeated(idRestaurant,reservationId, seated);
+	public ResponseEntity<?> markReservationSeated(@PathVariable Long idRestaurant, @PathVariable Long reservationId, @RequestParam Boolean seated) {
+		reservationService.markReservationSeated(idRestaurant, reservationId, seated);
 		return ResponseEntity.ok().build();
 	}
 
+	/**
+	 * Retrieves the current restaurant from the security context.
+	 *
+	 * @return the current restaurant
+	 */
 	private Restaurant getCurrentRestaurant() {
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		if (principal instanceof User) {
