e431279b3d133db5c2617ae618faa5567c8c57d9
Valentino <ascolesevalentino@gmail.com>
2025-02-03 22:44:18 +0100

    Add @Lazy annotation to RestaurantUserService injection in RestaurantService for improved dependency management

--- greedys_api/src/main/java/com/application/service/RestaurantService.java
+++ greedys_api/src/main/java/com/application/service/RestaurantService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -55,6 +56,7 @@ public class RestaurantService {
 	private RestaurantRoleDAO restaurantRoleDAO;
 
 	@Autowired
+	@Lazy
 	private RestaurantUserService restaurantUserService;
 
 	public Restaurant getReference(Long id) {
