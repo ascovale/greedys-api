1751635c0f92ea91936e6446b74fcf06c1091ca9
ascovale <167000863+ascovale@users.noreply.github.com>
2025-03-11 22:03:07 +0100

    Fixed AdminRequestFilter, CustomerRequestFilter, RestaurantUserRequestFilter

--- greedys_api/src/main/java/com/application/spring/requestfilter/CustomerRequestFilter.java
+++ greedys_api/src/main/java/com/application/spring/requestfilter/CustomerRequestFilter.java
@@ -1,17 +1,59 @@
 package com.application.spring.requestfilter;
 
-import org.springframework.stereotype.Component;
+import java.io.IOException;
 
-import com.application.security.jwt.JwtRequestFilter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import com.application.security.jwt.JwtUtil;
 import com.application.security.user.customer.CustomerUserDetailsService;
 
-@Component
-public class CustomerRequestFilter extends JwtRequestFilter {
-    CustomerUserDetailsService userDetailsService;
-    JwtUtil passwordEncoder;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-    public CustomerRequestFilter(CustomerUserDetailsService userDetailsService, JwtUtil passwordEncoder) {
-        super(userDetailsService, passwordEncoder);
+@Service
+public class CustomerRequestFilter extends OncePerRequestFilter {
+
+    private CustomerUserDetailsService userDetailsService;
+    private JwtUtil jwtUtil;
+
+    public CustomerRequestFilter(CustomerUserDetailsService userDetailsService, JwtUtil jwtUtil) {
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        chain.doFilter(request, response);
     }
 }
