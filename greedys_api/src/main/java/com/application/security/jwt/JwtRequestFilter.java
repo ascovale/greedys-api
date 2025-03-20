af16e6deff7adf4aa16fc1dc102b2d07809cd01e
Matteo <matteo.rossi1902@gmail.com>
2025-03-20 15:25:32 +0100

    Enhance JWT handling by updating key initialization and modifying method visibility in JwtUtil

--- greedys_api/src/main/java/com/application/security/jwt/JwtRequestFilter.java
+++ greedys_api/src/main/java/com/application/security/jwt/JwtRequestFilter.java
@@ -48,6 +48,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 usernamePasswordAuthenticationToken
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                
             }
         }
         chain.doFilter(request, response);
