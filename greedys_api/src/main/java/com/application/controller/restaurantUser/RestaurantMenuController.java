971a97dfb2047b3343a0bc8bd5a418319735ad59
ascovale <167000863+ascovale@users.noreply.github.com>
2025-03-20 21:05:25 +0100

    Refactor Customer and Admin models to use status enums, update SlotController path variable, and implement role management in RestaurantUserService

--- greedys_api/src/main/java/com/application/controller/restaurantUser/RestaurantMenuController.java
+++ greedys_api/src/main/java/com/application/controller/restaurantUser/RestaurantMenuController.java
@@ -3,13 +3,14 @@ package com.application.controller.restaurantUser;
 import java.util.Collection;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.application.controller.utils.ControllerUtils;
 import com.application.service.RestaurantMenuService;
 import com.application.web.dto.get.MenuItemDTO;
 import com.application.web.dto.get.PricedMenuItemDTO;
@@ -23,44 +24,48 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Menu", description = "Restaurant Menu Controller APIs")
 @SecurityRequirement(name = "restaurantBearerAuth")
-//@RequestMapping("/restaurant/services")
-@RequestMapping("/restaurant_user/{idRestaurantUser}")
-//@PreAuthorize("@securityService.isRestaurantUserPermission(#idRestaurantUser)")
+@RequestMapping("/restaurant/menu")
 @RestController
 public class RestaurantMenuController {
+    
     //TODO: sistemare i metodi
-
     private final RestaurantMenuService restaurantMenuService;
 
     public RestaurantMenuController(RestaurantMenuService restaurantMenuService) {
         this.restaurantMenuService = restaurantMenuService;
     }
 
+    @PreAuthorize("authentication.principal.isEnabled()")
     @GetMapping("/restaurant/menu")
-    public Collection<RestaurantMenuDTO> getRestaurantMenus( @RequestParam Long id) {
-        return restaurantMenuService.getMenusByRestaurant(id);
+    public Collection<RestaurantMenuDTO> getRestaurantMenus() {
+        return restaurantMenuService.getMenusByRestaurant(ControllerUtils.getCurrentRestaurant().getId());
     }
 
+	@PreAuthorize("authentication.principal.isEnabled()")
     @GetMapping("/menu/item")
-    public Collection<PricedMenuItemDTO> getMenuItems( @RequestParam Long id) {
-        return restaurantMenuService.getMenuItems(id);
+    public Collection<PricedMenuItemDTO> getMenuItems() {
+        return restaurantMenuService.getMenuItems(ControllerUtils.getCurrentRestaurant().getId());
     }
 
+	@PreAuthorize("authentication.principal.isEnabled()")
     @GetMapping("/restaurant/item")
-    public Collection<MenuItemDTO> getRestaurantMenuItems( @RequestParam Long id) {
-        return restaurantMenuService.getMenuItemsByRestaurant(id);
+    public Collection<MenuItemDTO> getRestaurantMenuItems() {
+        return restaurantMenuService.getMenuItemsByRestaurant(ControllerUtils.getCurrentRestaurant().getId());
     }
 
+	@PreAuthorize("authentication.principal.isEnabled()")
     @PostMapping("/menu")
     public void createMenu(@RequestBody NewRestaurantMenuDTO newMenu) {
         restaurantMenuService.addMenu(newMenu);
     }
 
+	@PreAuthorize("authentication.principal.isEnabled()")
     @PostMapping("/menu/item")
     public void addItemToMenu(@RequestBody NewPricedMenuItemDTO newMenuItem) {
         restaurantMenuService.addPricedMenuItem(newMenuItem);
     }
 
+	@PreAuthorize("authentication.principal.isEnabled()")
     @PostMapping("/item")
     public ResponseEntity<Void> createItem(@RequestBody NewMenuItemDTO newItem) {
         System.out.println(newItem.getName() + " " + newItem.getDescription() + " " + newItem.getRestaurantId());
