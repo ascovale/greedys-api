4f482e3541e03dd6c8cf4d373fd342baf027fd46
Matteo <matteo.rossi1902@gmail.com>
2025-05-11 17:01:08 +0200

    fix: Update Docker image reference in deploy script to use the correct path

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 echo "Pulling the latest Docker image..."
-docker pull registry.gitlab.com/psychoorange/greedys_api/spring-app:latest
+docker pull registry.gitlab.com/psychoorange/greedys_api:latest
 
 echo "Removing the existing service..."
 docker service rm greedys_api_spring-app || echo "Service not found, skipping removal."
