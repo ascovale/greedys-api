c85acd16874a907e0c84e7e61c9e172d9f32ebd6
ascovale <167000863+ascovale@users.noreply.github.com>
2025-06-17 23:08:28 +0200

    feat: update restaurant selection process to include JWT token handling

--- greedys_api/src/main/java/com/application/web/dto/post/RestaurantUserSelectRequestDTO.java
+++ greedys_api/src/main/java/com/application/web/dto/post/RestaurantUserSelectRequestDTO.java
@@ -1,10 +1,7 @@
 package com.application.web.dto.post;
 
 public class RestaurantUserSelectRequestDTO {
-    private String hubToken;
     private Long restaurantId;
-    public String getHubToken() { return hubToken; }
-    public void setHubToken(String hubToken) { this.hubToken = hubToken; }
     public Long getRestaurantId() { return restaurantId; }
     public void setRestaurantId(Long restaurantId) { this.restaurantId = restaurantId; }
 }
