3e7c91db234aca5f174622309436c80c27ea78ab
ascolesevalentino <167000863+ascovale@users.noreply.github.com>
2025-08-20 00:58:12 +0200

    feat: Remove unused mapping methods from mapper interfaces to streamline code

--- greedys_api/src/main/java/com/application/common/persistence/mapper/DishMapper.java
+++ greedys_api/src/main/java/com/application/common/persistence/mapper/DishMapper.java
@@ -38,34 +38,8 @@ public interface DishMapper {
     @Mapping(target = "menus", ignore = true)
     @Mapping(target = "restaurant", ignore = true) // Verrà impostato dal service
     @Mapping(target = "allergens", ignore = true)
-    Dish fromNewDishDTO(NewDishDTO newDishDTO);
+  Dish fromNewDishDTO(NewDishDTO newDishDTO);
 
-    /**
-     * Converte un DishDTO in entità Dish
-     */
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "photos", ignore = true)
-    @Mapping(target = "photoLinks", ignore = true)
-    @Mapping(target = "categories", ignore = true)
-    @Mapping(target = "ingredients", ignore = true)
-    @Mapping(target = "menuDishes", ignore = true)
-    @Mapping(target = "menus", ignore = true)
-    @Mapping(target = "restaurant", ignore = true)
-    Dish toEntity(DishDTO dishDTO);
-
-    /**
-     * Aggiorna un'entità Dish esistente con i dati dal DishDTO
-     */
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "photos", ignore = true)
-    @Mapping(target = "photoLinks", ignore = true)
-    @Mapping(target = "categories", ignore = true)
-    @Mapping(target = "ingredients", ignore = true)
-    @Mapping(target = "menuDishes", ignore = true)
-    @Mapping(target = "menus", ignore = true)
-    @Mapping(target = "restaurant", ignore = true)
-    @Mapping(target = "allergens", ignore = true)
-    void updateEntityFromDTO(DishDTO dto, @MappingTarget Dish entity);
 
     /**
      * Aggiorna un'entità Dish esistente con i dati dal NewDishDTO
--- greedys_api/src/main/java/com/application/common/persistence/mapper/RestaurantMapper.java
+++ greedys_api/src/main/java/com/application/common/persistence/mapper/RestaurantMapper.java
@@ -47,40 +47,6 @@ public interface RestaurantMapper {
     Restaurant fromNewRestaurantDTO(NewRestaurantDTO newRestaurantDTO);
 
     /**
-     * Converte un RestaurantDTO in entità Restaurant
-     */
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "postCode", source = "post_code")
-    @Mapping(target = "vatNumber", source = "vatNumber")
-    @Mapping(target = "creationDate", ignore = true) // Sarà impostato dal service
-    @Mapping(target = "status", ignore = true) // Sarà impostato dal service
-    @Mapping(target = "services", ignore = true) // Non mappato nei DTO base
-    @Mapping(target = "dishes", ignore = true) // Non mappato nei DTO base
-    @Mapping(target = "RUsers", ignore = true) // Non mappato nei DTO base
-    @Mapping(target = "restaurantImages", ignore = true) // Non mappato nei DTO base
-    @Mapping(target = "latitude", ignore = true) // Non mappato nei DTO base
-    @Mapping(target = "longitude", ignore = true) // Non mappato nei DTO base
-    @Mapping(target = "restaurantTypes", ignore = true) // Non mappato nei DTO base
-    Restaurant toEntity(RestaurantDTO restaurantDTO);
-
-    /**
-     * Aggiorna un'entità Restaurant esistente con i dati dal RestaurantDTO
-     */
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "postCode", source = "post_code")
-    @Mapping(target = "vatNumber", source = "vatNumber")
-    @Mapping(target = "creationDate", ignore = true)
-    @Mapping(target = "status", ignore = true)
-    @Mapping(target = "services", ignore = true)
-    @Mapping(target = "dishes", ignore = true)
-    @Mapping(target = "RUsers", ignore = true)
-    @Mapping(target = "restaurantImages", ignore = true)
-    @Mapping(target = "latitude", ignore = true)
-    @Mapping(target = "longitude", ignore = true)
-    @Mapping(target = "restaurantTypes", ignore = true)
-    void updateEntityFromDTO(RestaurantDTO dto, @MappingTarget Restaurant entity);
-
-    /**
      * Aggiorna un'entità Restaurant esistente con i dati dal NewRestaurantDTO
      */
     @Mapping(target = "id", ignore = true)
