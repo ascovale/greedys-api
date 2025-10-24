70efc846d4717d61242c8b4d175a85a945f71780
ascolesevalentino <167000863+ascovale@users.noreply.github.com>
2025-10-24 19:29:28 +0200

    ci: Enhance deployment process by adding Traefik config transfer and setup

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,15 @@ set -e  # Exit immediately if a command exits with a non-zero status
 echo "Pulling the latest Docker image..."
 docker pull registry.gitlab.com/greedysgroup/greedys_api:latest
 
+echo "Checking and creating traefik directory..."
+mkdir -p traefik
+
+echo "Creating acme.json if it doesn't exist..."
+if [ ! -f traefik/acme.json ]; then
+  touch traefik/acme.json
+  chmod 600 traefik/acme.json
+fi
+
 echo "Removing the existing stack..."
 docker stack rm greedys_api || echo "Stack not found, skipping removal."
 
